5b9c985ea6ce2d7036e849663d2d1f52ccf359b2
Eric <e2zou@uwaterloo.ca>
2026-03-11 07:30:35 -0400

    day 3 progress: Learned macros in c. defined row and table structs and schemas Learned how much work it goes into calculating the exact byte offset for rows. Learned serialization and deseieralization learned how sscanf works.

--- main.c
+++ main.c
@@ -10,9 +10,29 @@ typedef enum {META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND} MetaComma
 
 typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_COMMAND} PrepareResult;
 
+// So these 2 are macros.
+// they basically replace the string "COLUMN_USERNAME_SIZE" with the number 32
+// they are similar to .env files that contain non secrets where you can easily tune them as its 1 place of truth
+// its good standard to have fixed size username and email -> fixed size row for easy calculate offset and next row for a db
+
+//Use this instead of a global variable for a FIXED size array. Because the macro exists during compilation, before
+//running and changing C to machine code.
+
+// in C, the size of an array inside a struct must be known at compile time.
+// since macros get deleted they dont take up any space, unlike a global int variable which takes up 4 bytes in RAM
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct{
+	uint32_t id;
+	char username [COLUMN_USERNAME_SIZE];
+	char email [COLUMN_EMAIL_SIZE];
+}Row;
+
 typedef enum {STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
 typedef struct {
 	StatementType type;
+	Row row_to_insert; 
 	
 }Statement;
 typedef struct{
@@ -20,6 +40,48 @@ typedef struct{
 	size_t buffer_length;
 	ssize_t input_length;
 }InputBuffer;
+
+//defining a table struct
+// 4096 to match with virtual memory systems of most os for efficient data transfer
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROWSIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct{
+	uint32_t num_rows;
+	void* pages[TABLE_MAX_PAGES];
+}Table;
+
+// defining a row schema
+
+
+#define size_of_attribute(Struct, Attribute) sizeof (((Struct*)0) -> Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+
+// read/write in memory for a particular row:
+
+void* row_slot(Table*table, uint32_t row_num){
+	uint32_t page_num = row_num/ROWS_PER_PAGE;
+	void* page = table-> pages[page_num];
+	if (page == NULL){
+		//allocate memory only when we try to access the page
+	}
+	uint32_t row_offset = row_num % ROWS_PER_PAGE;
+	uint32_t byte_offset = row_offset * ROW_SIZE;
+	return page + byte_offset;
+}
+
+
 
 
 
@@ -29,6 +91,20 @@ InputBuffer* new_input_buffer(){
 	input_buffer -> buffer_length = 0;
 	input_buffer -> input_length = 0;
 	return input_buffer;
+}
+//serialize: taking object and put it into raw stream of bytes
+// how it works here: take wahtever's at the memory of source of size size, and put it at the right destination
+// use void pointer to be adaptable on what is there at the destination.
+void serialize_row(Row* source, void* destination) {
+	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+	memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+	memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+	memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+	memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+	memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 //free memory
@@ -52,6 +128,12 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
 	if (strncmp(input_buffer -> buffer, "insert", 6) == 0){
 		statement->type = STATEMENT_INSERT;
+		int args_assigned = sscanf(
+			input_buffer -> buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+			statement->row_to_insert.username, statement-> row_to_insert.email);
+		if (args_assigned < 3){
+			return PREPARE_SYNTAX_ERROR;
+		}
 		return PREPARE_SUCCESS;
 	}
 	if (strcmp(input_buffer -> buffer, "select") == 0){
