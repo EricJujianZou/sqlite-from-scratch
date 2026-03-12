a868b9c6f47622eb586947b27498f6e73619f864
Eric <e2zou@uwaterloo.ca>
2026-03-12 07:38:07 -0400

    Day 4 progress: Added table definition, initialization, and edited previous statements to take in to account of the table param.

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 // There's 2 kinds of commands for sqlite. 1st is meta command which is not a sql query keyword
 // 2nd type is a prepare command which is a keywrod like insert or select
 // statement type is to distinguish between the two.
 typedef enum {META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND} MetaCommandResult;
 
-typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_COMMAND} PrepareResult;
+typedef enum {PREPARE_SUCCESS, PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_COMMAND} PrepareResult;
 
 // So these 2 are macros.
 // they basically replace the string "COLUMN_USERNAME_SIZE" with the number 32
@@ -29,6 +30,8 @@ typedef struct{
 	char email [COLUMN_EMAIL_SIZE];
 }Row;
 
+typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+
 typedef enum {STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
 typedef struct {
 	StatementType type;
@@ -40,19 +43,6 @@ typedef struct{
 	size_t buffer_length;
 	ssize_t input_length;
 }InputBuffer;
-
-//defining a table struct
-// 4096 to match with virtual memory systems of most os for efficient data transfer
-const uint32_t PAGE_SIZE = 4096;
-#define TABLE_MAX_PAGES 100
-
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROWSIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-
-typedef struct{
-	uint32_t num_rows;
-	void* pages[TABLE_MAX_PAGES];
-}Table;
 
 // defining a row schema
 
@@ -66,6 +56,24 @@ const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+
+//defining a table struct
+// 4096 to match with virtual memory systems of most os for efficient data transfer
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct{
+	uint32_t num_rows;
+	void* pages[TABLE_MAX_PAGES];
+}Table;
+
+void print_row(Row*row){
+	printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
 
 // read/write in memory for a particular row:
@@ -82,16 +90,6 @@ void* row_slot(Table*table, uint32_t row_num){
 }
 
 
-
-
-
-InputBuffer* new_input_buffer(){
-	InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-	input_buffer -> buffer = NULL;
-	input_buffer -> buffer_length = 0;
-	input_buffer -> input_length = 0;
-	return input_buffer;
-}
 //serialize: taking object and put it into raw stream of bytes
 // how it works here: take wahtever's at the memory of source of size size, and put it at the right destination
 // use void pointer to be adaptable on what is there at the destination.
@@ -107,6 +105,39 @@ void deserialize_row(void* source, Row* destination) {
 	memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+
+// initializing a table which is basically a pointer to 100 pointers.
+
+Table* new_table(){
+	Table* table = (Table*)malloc(sizeof(Table));
+	table->num_rows = 0;
+	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++){
+		// cleaning up garbage values to null.
+		table->pages[i] = NULL;
+	}
+	return table;
+}
+
+void free_table(Table* table){
+	for (int i = 0; i<TABLE_MAX_PAGES; i++){
+		if (table->pages[i]){
+			free(table -> pages[i]);
+		}
+	}
+	free(table);
+}
+
+
+
+
+
+InputBuffer* new_input_buffer(){
+	InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+	input_buffer -> buffer = NULL;
+	input_buffer -> buffer_length = 0;
+	input_buffer -> input_length = 0;
+	return input_buffer;
+}
 //free memory
 void close_input_buffer(InputBuffer* input_buffer){
 	free(input_buffer->buffer);
@@ -114,9 +145,10 @@ void close_input_buffer(InputBuffer* input_buffer){
 }
 
 // wrapping .exit which is our only meta command but allowing ability to add more later on
-MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table){
 	if (strcmp(input_buffer->buffer, ".exit") == 0){
 		close_input_buffer(input_buffer);
+		free_table(table);
 		exit(EXIT_SUCCESS);
 	}
 	else {
@@ -143,17 +175,36 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 	return PREPARE_UNRECOGNIZED_COMMAND;
 }
 
-void execute_statement(Statement*statement){
+ExecuteResult execute_insert(Statement*statement, Table *table){
+	if (table->num_rows >= TABLE_MAX_ROWS){
+		return EXECUTE_TABLE_FULL;
+
+	}
+	Row* row_to_insert = &(statement->row_to_insert);
+	serialize_row(row_to_insert, row_slot(table, table-> num_rows));
+	table->num_rows+=1;
+	return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_select(Statement* statement, Table* table){
+	Row row;
+	for (uint32_t i = 0; i < table-> num_rows; i++){
+		deserialize_row (row_slot(table, i), &row);
+		print_row(&row);
+	}
+	return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_statement(Statement* statement, Table *table){
 	switch (statement->type){
 		case(STATEMENT_INSERT):
-			printf("inserting.\n");
-			break;
+			return execute_insert(statement, table);
 		case(STATEMENT_SELECT):
-			printf("well be selecting.\n");
-			break;
+			return execute_select(statement, table);
 		
 	}
 }
+
+	
+
 
 void print_prompt(){
 	printf("db > ");
@@ -174,13 +225,14 @@ void read_input(InputBuffer * input_buffer){
 
 
 int main(int argc, char* argv[]){
+	Table* table = new_table();
 	InputBuffer* input_buffer = new_input_buffer();
 	while(true){
 		print_prompt();
 		read_input(input_buffer);
 
 		if (input_buffer -> buffer[0] == '.'){
-			switch (do_meta_command(input_buffer)){
+			switch (do_meta_command(input_buffer, table)){
 				case(META_COMMAND_SUCCESS):
 					continue;
 				case (META_COMMAND_UNRECOGNIZED_COMMAND):
@@ -192,11 +244,20 @@ int main(int argc, char* argv[]){
 		switch (prepare_statement(input_buffer, &statement)){
 			case (PREPARE_SUCCESS):
 				break;
+			case (PREPARE_SYNTAX_ERROR):
+				printf("Syntax error. Could not parse satement. \n");
+				continue;
 			case (PREPARE_UNRECOGNIZED_COMMAND):
 				printf("unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
 				continue;
 		}
-		execute_statement(&statement);
-		printf("Executed.\n");
+		switch (execute_statement(&statement, table)){
+			case (EXECUTE_SUCCESS):
+				printf("Executed. \n");
+				break;
+			case(EXECUTE_TABLE_FULL):
+				printf("Error: table full .\n");
+				break;
+		}
 	}
 }
